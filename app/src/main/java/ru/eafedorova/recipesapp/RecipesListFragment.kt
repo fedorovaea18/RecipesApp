8cd02dcb73dfe6887b722ed96e3d0031ce0ad3b7
fedorovaea18 <fedorovaea18@yandex.ru>
2025-05-27 19:37:47 +0300

    RA-12: implemented the Parcelable interface and passed the Recipe object, fixed the rounding of the cards

--- app/src/main/java/ru/eafedorova/recipesapp/RecipesListFragment.kt
+++ app/src/main/java/ru/eafedorova/recipesapp/RecipesListFragment.kt
@@ -1,13 +1,18 @@
 package ru.eafedorova.recipesapp
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import ru.eafedorova.recipesapp.databinding.FragmentListRecipesBinding
+import java.io.IOException
+import java.io.InputStream
 
 class RecipesListFragment : Fragment() {
 
@@ -20,13 +25,38 @@ class RecipesListFragment : Fragment() {
     private var categoryName: String? = null
     private var categoryImageUrl: String? = null
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    companion object {
+        const val ARG_RECIPE = "arg_recipe"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         categoryId = requireArguments().getInt(CategoriesListFragment.ARG_CATEGORY_ID)
         categoryName = requireArguments().getString(CategoriesListFragment.ARG_CATEGORY_NAME)
         categoryImageUrl =
             requireArguments().getString(CategoriesListFragment.ARG_CATEGORY_IMAGE_URL)
 
+    }
+
+    private fun getHeaderCategory() {
+
+        binding.tvTitleCategoryRecipe.text = categoryName
+
+        val drawable = try {
+            val inputStream: InputStream? =
+                categoryImageUrl?.let { binding.root.context.assets.open(it) }
+            Drawable.createFromStream(inputStream, null)
+        } catch (e: IOException) {
+            Log.e("RecipeListAdapter", "Ошибка при загрузке изображения: ${e.message}", e)
+            null
+        }
+
+        binding.ivImageCategoryRecipe.setImageDrawable(drawable)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getHeaderCategory()
         initRecycler()
     }
 
@@ -38,6 +68,7 @@ class RecipesListFragment : Fragment() {
 
         _binding = FragmentListRecipesBinding.inflate(inflater)
         return binding.root
+
     }
 
     override fun onDestroyView() {
@@ -57,8 +88,10 @@ class RecipesListFragment : Fragment() {
     }
 
     private fun openRecipeByRecipeId(recipeId: Int) {
+        val recipe = STUB.getRecipeById(recipeId)
+        val bundle = bundleOf(ARG_RECIPE to recipe)
         parentFragmentManager.commit {
-            replace<RecipeFragment>(R.id.mainContainer)
+            replace<RecipeFragment>(R.id.mainContainer, args = bundle)
             setReorderingAllowed(true)
             addToBackStack(null)
         }
